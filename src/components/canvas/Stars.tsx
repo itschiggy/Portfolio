b5c057d9002f3eb378964a9e59ecd1fdf42250df
itschiggy <itschiragtaneja@gmail.com>
2024-02-14 00:26:13 +0530

    supressed ts-error for maath library

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -1,6 +1,7 @@
 import {useState, useRef, Suspense} from "react";
 import {Canvas, useFrame} from "@react-three/fiber";
 import {Points, PointMaterial, Preload} from "@react-three/drei";
+// @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'maath...
 import * as random from "maath/random/dist/maath-random.esm";
 import * as THREE from 'three';
 
